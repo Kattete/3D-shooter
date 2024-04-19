2b58a8f8d207f07cf19b00c52e5eaf8be510dec4
Kattete <88459780+Kattete@users.noreply.github.com>
2024-04-19 11:32:53 +0300

    Bullet destroying

--- 3D Shooter/Assets/Bullet.cs
+++ 3D Shooter/Assets/Bullet.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private void Update()
+    {
+        Invoke("DestroyItem", 10f);
+    }
+
     private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void DestroyItem()
     {
         Destroy(gameObject);
     }
